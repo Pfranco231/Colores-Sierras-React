a3f079f40f08894d6426f6dff882b5e9e38079a1
Franco_Vidanatural <Franco Papeschi>
2025-08-30 14:11:59 -0300

    Hice parte del header

--- colores-sierras/src/componets/Navbar/Navbar.jsx
+++ colores-sierras/src/componets/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 
 const Navbar = () => {
     return(
         <>
-        
+        <header className="flex h-20 items-center p-10 justify-between sticky z-10">
+            <h6>Colores de las sierras</h6>
+            <ul className="flex items-center gap-3">
+                <li>
+                    <NavLink to="/">
+                        <a>Inicio</a>
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/contacto">
+                        <a>Contacto</a>
+                    </NavLink>
+                </li>
+            </ul>
+        </header>
         </>
     )
 }
