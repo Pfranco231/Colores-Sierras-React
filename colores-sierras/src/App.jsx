aaad592f2bca859340407572e873b96125b100f1
Franco_Vidanatural <Franco Papeschi>
2025-08-30 09:42:19 -0300

    Primer Commit de Colores de las sierras

---
+++ colores-sierras/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./componets/Navbar/Navbar";
+
+const App = () => {
+
+  return(
+    <>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+
+}
+
+export default App
